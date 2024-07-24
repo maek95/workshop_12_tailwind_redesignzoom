88339f31c4310941a8757668b9338cb910e562dc
maek95 <mekstrom01@gmail.com>
2024-07-24 16:06:22 +0200

    tripple image overlap, corrected a lot of spacing flex etc, gradient, and more

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,13 @@ export default {
         sans: ["'Inter'", ...defaultTheme.fontFamily.sans],
         mono: ["'Fira Code'", ...defaultTheme.fontFamily.mono],
         coolvetica: ['Coolvetica', 'sans-serif'],
+      },
+      height: {
+        'screen-minus-header': 'calc(100vh - 88px)',
+      },
+      backgroundImage: {
+        'white-darkblue-gradient': 'linear-gradient(to bottom, #FFFFFF 0%, #00031f 100%)',
+        'white-lightblue-gradient': 'linear-gradient(to bottom, #FFFFFF 0%, #FFFFFF 70%, #b5d0f8 100%)'
       }
     },
   },
